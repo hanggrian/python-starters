163dd1d24e2e9e275088e75649b041c75c02a032
Hendra Anggrian <hanggrian@proton.me>
2026-05-10 11:39:58 -0500

    Pin specific Ubuntu version, use clean install, remove Gradle flags

--- test_projects.sh
+++ test_projects.sh
@@ -13,38 +13,6 @@ APPLICATION="$(cd "$SOURCE_ROOT/application" && pwd)" && readonly APPLICATION
 LIBRARY="$(cd "$SOURCE_ROOT/library" && pwd)" && readonly LIBRARY
 NOTEBOOK="$(cd "$SOURCE_ROOT/notebook" && pwd)" && readonly NOTEBOOK
 
-install() {
-  uv -q sync
-  uv -q pip install -r website/requirements.txt
-}
-
-lint() {
-  uv run pylint "$@"
-  uv run stylebook .github/ *.toml
-}
-
-test() {
-  if [[ $# -eq 1 ]]; then
-    uv run pytest -q "$@" --cov
-  else
-    uv run pytest -q "$1" --cov="$1"
-    shift
-    for arg in "$@"; do
-      uv run pytest -q "$arg" --cov="$arg" --cov-append
-    done
-  fi
-  uv run coverage xml
-}
-
-build_docs() {
-  uv run pdoc -o build/pdoc/ "$@"
-}
-
-build_website() {
-  cd 'website' || exit 1
-  uv run mkdocs -q build
-}
-
 COVERAGE_FILE='coverage.xml' && readonly COVERAGE_FILE
 PDOC_DIR='build/pdoc/' && readonly PDOC_DIR
 MKDOCS_DIR='website/site/' && readonly MKDOCS_DIR
@@ -53,10 +21,10 @@ warn 'Testing application...'
 
 echo '(1/3) Running UV commands'
 cd "$APPLICATION" || exit 1
-install
-lint src/
-test tests/
-build_website
+uv sync
+uv run poe lint
+uv run poe coverage
+uv run poe build-website
 cd "$APPLICATION" || exit 1
 
 echo '(2/3) Checking coverage file'
@@ -73,12 +41,11 @@ warn 'Testing library...'
 
 echo '(1/4) Running UV commands'
 cd "$LIBRARY" || exit 1
-install
-# duplicate directory names, `uv run pylint library` wouldn't work
-lint library/library/ library/tests/ library-extension/
-test library/ library-extension/
-build_docs library/library/ library-extension/library_extension/
-build_website
+uv sync
+uv run poe lint
+uv run poe coverage
+uv run poe documentation
+uv run poe build-website
 cd "$LIBRARY" || exit 1
 
 echo '(2/4) Checking coverage file'
@@ -100,10 +67,10 @@ warn 'Testing notebook...'
 
 echo '(1/3) Running UV commands'
 cd "$NOTEBOOK" || exit 1
-install
-lint ./*.py
-build_docs ./*.py
-build_website
+uv sync
+uv run poe lint
+uv run poe documentation
+uv run poe build-website
 cd "$NOTEBOOK" || exit 1
 
 echo '(2/3) Checking documentation directory'
